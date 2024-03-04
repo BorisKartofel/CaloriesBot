e862824713fbffe3f1765a4c10a5dda4f118f192
BorisKartofel <botisov-anton-0@mail.ru>
2024-03-04 13:25:46 +0300

    Создано

---
+++ src/main/java/telegram/Calories_Bot/CaloriesBotApplication.java
@@ -0,0 +1,13 @@
+package telegram.Calories_Bot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaloriesBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CaloriesBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/telegram/Calories_Bot/CaloriesBotApplicationTests.java
@@ -0,0 +1,13 @@
+package telegram.Calories_Bot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CaloriesBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
